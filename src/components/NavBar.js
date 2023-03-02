60213c09d47a11711407968ea60414332e5c030c
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-03-02 14:16:30 +0000

    event listener added for user scroll

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
 import { Navbar, Container } from "react-bootstrap";
 
 export const NavBar = () => {
+    const [activeLink, setActiveLink] = useState('home');
+    const [scrolled, seScrolled] = useState(false);
+    //detect user scroll
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (window.scrollY > 50) {
+                seScrolled(true);
+            }
+            else {
+                seScrolled(false);
+            }
+        }
+
+        window.addEventListener("scroll", onScroll);
+
+        return () => window.removeEventListener("scroll", onScroll);
+
+    },[])
+    //add and removes event listener depending on whether or not user is scrolling through page
+
     return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? "scrolled": ""}>
       <Container>
         <Navbar.Brand href="#home">
             <img src={``} alt="Logo" />
